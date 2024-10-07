a08c96914997f132e1c506b0ce853598d6969e34
Ramesh <dev0pslearning2025@gmail.com>
2024-10-07 12:35:38 +0530

    Expense-shell-1

--- mysql.sh
+++ mysql.sh
@@ -7,20 +7,14 @@ echo "Enter the db root password"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>> $LOG
-#VALIDATE $? "The mysql installation is"
-
 systemctl enable mysqld &>> $LOG
-#VALIDATE $? "Enabled mysqld is"
-
 systemctl start mysqld &>> $LOG
-#VALIDATE $? "mysql start is"
-
 ##checking mysql root password setup
 mysql -h db.devopslearning2025.online -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>LOG
 
 if [ $? -ne 0 ]
 then 
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>> $LOG
+    mysql_secure_installationn --set-root-pass ${mysql_root_password} &>> $LOG
  #   VALIDATE $? "root password setting is"
 else
     echo -e "mysql password already setup $Y SKIPPING... $N"
